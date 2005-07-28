5f7d492a1c74c668595db0637c16aeba17bd2b75
Alex Thuering <alex@thuering.biz>
2005-07-28 20:00:46 +0000

    added GetElementById added use element

--- src/SVGDocument.cpp
+++ src/SVGDocument.cpp
@@ -3,7 +3,7 @@
 // Purpose:     wxSVGDocument - SVG render & data holder class
 // Author:      Alex Thuering
 // Created:     2005/01/17
-// RCS-ID:      $Id: SVGDocument.cpp,v 1.11 2005-06-20 13:29:26 ntalex Exp $
+// RCS-ID:      $Id: SVGDocument.cpp,v 1.12 2005-07-28 20:00:46 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 //////////////////////////////////////////////////////////////////////////////
@@ -37,112 +37,216 @@ wxXmlElement* wxSVGDocument::CreateElement(const wxString& tagName)
 
 #include "SVGDocument_CreateElement.cpp"
 
-void RenderChilds(wxSVGCanvas* canvas, wxSVGElement* parent,
+wxSVGElement* GetElemById(wxSVGElement* parent, const wxString& id)
+{
+  wxSVGElement* elem = (wxSVGElement*) parent->GetChildren();
+  while (elem)
+  {
+	if (elem->GetType() == wxXML_ELEMENT_NODE &&
+        elem->GetId() == id)
+      return elem;
+    if (elem->GetDtd() == wxSVG_G_ELEMENT ||
+        elem->GetDtd() == wxSVG_DEFS_ELEMENT ||
+        elem->GetDtd() == wxSVG_SYMBOL_ELEMENT)
+    {
+      wxSVGElement* elem2 = GetElemById(elem, id);
+      if (elem2 != NULL)
+        return elem2;
+    }
+    elem = (wxSVGElement*) elem->GetNext();
+  }
+  return NULL;
+}
+
+wxSVGElement* wxSVGDocument::GetElementById(const wxString& id)
+{
+  return GetRootElement() ? GetElemById(GetRootElement(), id) : NULL;
+}
+
+void RenderChilds(wxSVGDocument* doc, wxSVGElement* parent,
+  const wxSVGMatrix* parentMatrix, const wxCSSStyleDeclaration* parentStyle,
+  wxSVGSVGElement* ownerSVGElement, wxSVGElement* viewportElement);
+
+void RenderElement(wxSVGDocument* doc, wxSVGElement* elem,
+  const wxSVGMatrix* parentMatrix, const wxCSSStyleDeclaration* parentStyle,
+  wxSVGSVGElement* ownerSVGElement, wxSVGElement* viewportElement)
+{
+  wxSVGMatrix matrix(*parentMatrix);
+  wxCSSStyleRef style(*parentStyle);
+  elem->SetOwnerSVGElement(ownerSVGElement);
+  elem->SetViewportElement(viewportElement);
+  
+  switch (elem->GetDtd())
+  {
+	case wxSVG_SVG_ELEMENT:
+    {
+	  wxSVGSVGElement* element = (wxSVGSVGElement*) elem;
+	  // view box
+	  wxSVGRect viewbox = element->GetViewBox().GetAnimVal();
+      if (viewbox.GetWidth()>0 && viewbox.GetHeight()>0)
+      {
+        if (element->GetPreserveAspectRatio().GetAnimVal().GetAlign() == wxSVG_PRESERVEASPECTRATIO_NONE)
+          matrix = matrix.ScaleNonUniform(
+            element->GetWidth().GetAnimVal()/viewbox.GetWidth(),
+            element->GetHeight().GetAnimVal()/viewbox.GetHeight());
+        else
+        {
+          double scale = 1;
+          scale = element->GetWidth().GetAnimVal()/viewbox.GetWidth();
+    	  if (scale>element->GetHeight().GetAnimVal()/viewbox.GetHeight())
+    	    scale = element->GetHeight().GetAnimVal()/viewbox.GetHeight();
+  	      matrix = matrix.Scale(scale);
+          if (viewbox.GetX()!=0 || viewbox.GetY()!=0)
+    	    matrix = matrix.Translate(-viewbox.GetX(), -viewbox.GetY());
+        }
+      }
+	  RenderChilds(doc, elem, &matrix, &style, element, element);
+	  break;
+    }
+	case wxSVG_G_ELEMENT:
+	{
+	  wxSVGGElement* element = (wxSVGGElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  RenderChilds(doc, elem, &matrix, &style, ownerSVGElement, viewportElement);
+	  break;
+	}
+	case wxSVG_LINE_ELEMENT:
+	{
+	  wxSVGLineElement* element = (wxSVGLineElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawLine(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_POLYLINE_ELEMENT:
+	{
+	  wxSVGPolylineElement* element = (wxSVGPolylineElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawPolyline(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_POLYGON_ELEMENT:
+	{
+	  wxSVGPolygonElement* element = (wxSVGPolygonElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawPolygon(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_RECT_ELEMENT:
+	{
+	  wxSVGRectElement* element = (wxSVGRectElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawRect(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_CIRCLE_ELEMENT:
+	{
+	  wxSVGCircleElement* element = (wxSVGCircleElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawCircle(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_ELLIPSE_ELEMENT:
+	{
+	  wxSVGEllipseElement* element = (wxSVGEllipseElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawEllipse(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_PATH_ELEMENT:
+	{
+	  wxSVGPathElement* element = (wxSVGPathElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawPath(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_TSPAN_ELEMENT:
+	  break;
+	case wxSVG_TEXT_ELEMENT:
+	{
+	  wxSVGTextElement* element = (wxSVGTextElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawText(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_IMAGE_ELEMENT:
+	{
+	  wxSVGImageElement* element = (wxSVGImageElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  doc->GetCanvas()->DrawImage(element, &matrix, &style);
+	  break;
+	}
+	case wxSVG_USE_ELEMENT:
+    {
+      wxSVGUseElement* element = (wxSVGUseElement*) elem;
+	  element->UpdateMatrix(matrix);
+	  style.Add(element->GetStyle());
+	  wxString href = element->GetHref();
+	  if (href.length() == 0 || href[0] != wxT('#'))
+	    break;
+      href.Remove(0,1);
+      wxSVGElement* refElem = doc->GetElementById(href);
+      if (!refElem)
+        break;
+      wxSVGGElement* gElem = new wxSVGGElement(doc);
+      gElem->SetOwnerSVGElement(ownerSVGElement);
+      gElem->SetViewportElement(viewportElement);
+      gElem->SetTransform(element->GetTransform());
+      gElem->SetStyle(element->GetStyle());
+      gElem->Translate(element->GetX().GetAnimVal(), element->GetY().GetAnimVal());
+      if (refElem->GetDtd() == wxSVG_SYMBOL_ELEMENT)
+      {
+        wxSVGSVGElement* svgElem = new wxSVGSVGElement(doc);
+        if (element->GetWidth().GetAnimVal().GetUnitType() != wxSVG_LENGTHTYPE_UNKNOWN)
+          svgElem->SetWidth(element->GetWidth().GetAnimVal());
+        else
+          svgElem->SetWidth(wxSVGLength(wxSVG_LENGTHTYPE_PERCENTAGE, 100));
+        if (element->GetHeight().GetAnimVal().GetUnitType() != wxSVG_LENGTHTYPE_UNKNOWN)
+          svgElem->SetHeight(element->GetHeight().GetAnimVal());
+        else
+          svgElem->SetHeight(wxSVGLength(wxSVG_LENGTHTYPE_PERCENTAGE, 100));
+        svgElem->SetViewBox(((wxSVGSymbolElement*)refElem)->GetViewBox());
+        svgElem->SetPreserveAspectRatio(((wxSVGSymbolElement*)refElem)->GetPreserveAspectRatio());
+        wxSVGElement* child = (wxSVGElement*) refElem->GetChildren();
+        while (child)
+        {
+       	  if (elem->GetType() == wxXML_ELEMENT_NODE)
+       	    svgElem->AddChild(child->CloneNode());
+          child = (wxSVGElement*) child->GetNext();
+        }
+        gElem->AddChild(svgElem);
+      }
+      else
+        gElem->AddChild(refElem->CloneNode());
+      RenderElement(doc, gElem, &matrix, &style, ownerSVGElement, viewportElement);
+      delete gElem;
+      break;
+    }
+	default:
+	  break;
+  }
+}
+
+void RenderChilds(wxSVGDocument* doc, wxSVGElement* parent,
   const wxSVGMatrix* parentMatrix, const wxCSSStyleDeclaration* parentStyle,
   wxSVGSVGElement* ownerSVGElement, wxSVGElement* viewportElement)
 {
   wxSVGElement* elem = (wxSVGElement*) parent->GetChildren();
   while (elem)
   {
-	if (elem->GetType() == wxXML_ELEMENT_NODE)
-	{
-	  wxSVGMatrix matrix(*parentMatrix);
-	  wxCSSStyleRef style(*parentStyle);
-	  elem->SetOwnerSVGElement(ownerSVGElement);
-	  elem->SetViewportElement(viewportElement);
-	  
-	  switch (elem->GetDtd())
-	  {
-		case wxSVG_SVG_ELEMENT:
-		  RenderChilds(canvas, elem, &matrix, &style, (wxSVGSVGElement*) elem, elem);
-		  break;
-		case wxSVG_G_ELEMENT:
-		{
-		  wxSVGGElement* element = (wxSVGGElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  RenderChilds(canvas, elem, &matrix, &style, ownerSVGElement, viewportElement);
-		  break;
-		}
-		case wxSVG_LINE_ELEMENT:
-		{
-		  wxSVGLineElement* element = (wxSVGLineElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawLine(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_POLYLINE_ELEMENT:
-		{
-		  wxSVGPolylineElement* element = (wxSVGPolylineElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawPolyline(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_POLYGON_ELEMENT:
-		{
-		  wxSVGPolygonElement* element = (wxSVGPolygonElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawPolygon(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_RECT_ELEMENT:
-		{
-		  wxSVGRectElement* element = (wxSVGRectElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawRect(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_CIRCLE_ELEMENT:
-		{
-		  wxSVGCircleElement* element = (wxSVGCircleElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawCircle(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_ELLIPSE_ELEMENT:
-		{
-		  wxSVGEllipseElement* element = (wxSVGEllipseElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawEllipse(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_PATH_ELEMENT:
-		{
-		  wxSVGPathElement* element = (wxSVGPathElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawPath(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_TSPAN_ELEMENT:
-		  break;
-		case wxSVG_TEXT_ELEMENT:
-		{
-		  wxSVGTextElement* element = (wxSVGTextElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawText(element, &matrix, &style);
-		  break;
-		}
-		case wxSVG_IMAGE_ELEMENT:
-		{
-		  wxSVGImageElement* element = (wxSVGImageElement*) elem;
-		  element->UpdateMatrix(matrix);
-		  style.Add(element->GetStyle());
-		  canvas->DrawImage(element, &matrix, &style);
-		  break;
-		}
-		default:
-		  break;
-	  }
-	}
-	elem = (wxSVGElement*) elem->GetNext();
+    if (elem->GetType() == wxXML_ELEMENT_NODE)
+      RenderElement(doc, elem, parentMatrix, parentStyle,
+        ownerSVGElement, viewportElement);
+    elem = (wxSVGElement*) elem->GetNext();
   }
 }
 
@@ -153,45 +257,61 @@ wxImage wxSVGDocument::Render(int width, int height)
   
   wxSVGMatrix matrix;
   
-  // view box
-  wxSVGRect viewbox = GetRootElement()->GetViewBox();
-  if (viewbox.GetWidth()<=0 && viewbox.GetHeight()<=0)
-    viewbox = wxSVGRect(0, 0, GetRootElement()->GetWidth().GetBaseVal(),
-					    GetRootElement()->GetHeight().GetBaseVal());
-  if (viewbox.GetWidth()<=0 || viewbox.GetHeight()<=0)
+  if (GetRootElement()->GetWidth().GetAnimVal().GetUnitType() == wxSVG_LENGTHTYPE_UNKNOWN ||
+      GetRootElement()->GetHeight().GetAnimVal().GetUnitType() == wxSVG_LENGTHTYPE_UNKNOWN)
   {
-	viewbox = GetRootElement()->GetBBox();
+    wxSVGRect bbox = GetRootElement()->GetBBox();
+    GetRootElement()->SetWidth(wxSVGLength(bbox.GetWidth()));
+    GetRootElement()->SetHeight(wxSVGLength(bbox.GetHeight()));
 	matrix = matrix.Scale(0.95).Translate(width*0.025, height*0.025);
   }
   
-  // scale it to fit in
-  double scale = 1;
   if (width == -1 || height == -1)
   {
-	width = (int) GetRootElement()->GetWidth().GetBaseVal();
-	height = (int) GetRootElement()->GetHeight().GetBaseVal();
+	width = (int) GetRootElement()->GetWidth().GetAnimVal();
+	height = (int) GetRootElement()->GetHeight().GetAnimVal();
 	if (width <= 0 || height <= 0)
 	{
-	  width = (int) viewbox.GetWidth();
-	  height = (int) viewbox.GetHeight();
+	  width = (int) GetRootElement()->GetViewBox().GetAnimVal().GetWidth();
+	  height = (int) GetRootElement()->GetViewBox().GetAnimVal().GetHeight();
 	}
   }
-  else if (viewbox.GetWidth()>0 && viewbox.GetHeight()>0)
+  
+  if (GetRootElement()->GetWidth().GetAnimVal().GetUnitType() == wxSVG_LENGTHTYPE_PERCENTAGE)
   {
-	scale = width/viewbox.GetWidth();
-	if (scale>height/viewbox.GetHeight())
-	  scale = height/viewbox.GetHeight();
+    wxSVGAnimatedLength l = GetRootElement()->GetWidth();
+    l.GetBaseVal().ToViewportWidth(width);
+    if (l.GetBaseVal() != ((const wxSVGAnimatedLength&) l).GetAnimVal())
+      l.GetAnimVal().ToViewportWidth(width);
+    GetRootElement()->SetWidth(l);
   }
-  matrix = matrix.Scale(scale);
-  if (viewbox.GetX()!=0 || viewbox.GetY()!=0)
-	matrix = matrix.Translate(-viewbox.GetX(), -viewbox.GetY());
+  
+  if (GetRootElement()->GetHeight().GetAnimVal().GetUnitType() == wxSVG_LENGTHTYPE_PERCENTAGE)
+  {
+    wxSVGAnimatedLength l = GetRootElement()->GetHeight();
+    l.GetBaseVal().ToViewportHeight(height);
+    if (l.GetBaseVal() != ((const wxSVGAnimatedLength&) l).GetAnimVal())
+      l.GetAnimVal().ToViewportHeight(height);
+    GetRootElement()->SetHeight(l);
+  }
+  
+  // scale it to fit in
+  if (GetRootElement()->GetWidth().GetAnimVal()>0 &&
+      GetRootElement()->GetHeight().GetAnimVal()>0)
+  {
+    double scale = 1;
+	scale = width/GetRootElement()->GetWidth().GetAnimVal();
+	if (scale > height/GetRootElement()->GetHeight().GetAnimVal())
+	  scale = height/GetRootElement()->GetHeight().GetAnimVal();
+    matrix = matrix.Scale(scale);
+  }
   
   // render
   wxImage image(width, height);
   m_canvas->SetImage(&image);
   m_canvas->Clear();
-  RenderChilds(m_canvas, GetRootElement(), &matrix,
-	&GetRootElement()->GetStyle(), GetRootElement(), GetRootElement());
+  RenderElement(this, GetRootElement(), &matrix,
+	&GetRootElement()->GetStyle(), NULL, NULL);
   
   return image;
 }
