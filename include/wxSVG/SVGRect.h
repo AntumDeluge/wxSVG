50113f5b2cd3078adb6e08a2617ac79bb84ed93e
Alex Thuering <alex@thuering.biz>
2005-09-25 11:45:26 +0000

    added IsEmpty()

--- include/wxSVG/SVGRect.h
+++ include/wxSVG/SVGRect.h
@@ -11,29 +11,34 @@
 
 class wxSVGRect
 {
+  public:
+
   protected:
     double m_x;
     double m_y;
     double m_width;
     double m_height;
+    bool m_empty;
 
   public:
     inline double GetX() const { return m_x; }
-    inline void SetX(double n) { m_x = n; }
+    inline void SetX(double n) { m_x = n; m_empty = false; }
 
     inline double GetY() const { return m_y; }
-    inline void SetY(double n) { m_y = n; }
+    inline void SetY(double n) { m_y = n; m_empty = false; }
 
     inline double GetWidth() const { return m_width; }
-    inline void SetWidth(double n) { m_width = n; }
+    inline void SetWidth(double n) { m_width = n; m_empty = false; }
 
     inline double GetHeight() const { return m_height; }
-    inline void SetHeight(double n) { m_height = n; }
-
+    inline void SetHeight(double n) { m_height = n; m_empty = false; }
+    
+    inline bool IsEmpty() { return m_empty; }
+    
   public:
-    wxSVGRect(): m_x(0), m_y(0), m_width(0), m_height(0) {}
+    wxSVGRect(): m_x(0), m_y(0), m_width(0), m_height(0), m_empty(true) {}
     wxSVGRect(double x, double y, double width, double height):
-      m_x(x), m_y(y), m_width(width), m_height(height) {}
+      m_x(x), m_y(y), m_width(width), m_height(height), m_empty(false) {}
 };
 
 #endif // WX_SVG_RECT_H
