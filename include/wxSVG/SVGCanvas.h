c08ba08d12f400e3213c9d501f34000de0da7948
Alex Thuering <alex@thuering.biz>
2006-01-07 20:44:09 +0000

    added support of gradients

--- include/wxSVG/SVGCanvas.h
+++ include/wxSVG/SVGCanvas.h
@@ -3,7 +3,7 @@
 // Purpose:     wxSVGCanvas - Base class for SVG renders (backends)
 // Author:      Alex Thuering
 // Created:     2005/05/02
-// RCS-ID:      $Id: SVGCanvas.h,v 1.6 2005-11-17 17:51:24 ntalex Exp $
+// RCS-ID:      $Id: SVGCanvas.h,v 1.7 2006-01-07 20:44:08 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 /////////////////////////////////////////////////////////////////////////////
@@ -11,13 +11,15 @@
 #ifndef WX_SVG_CANVAS_H
 #define WX_SVG_CANVAS_H
 
+class wxSVGDocument;
+
 #include "SVGCanvasItem.h"
 
 /** Base class for SVG-Renders (backends) */
 class wxSVGCanvas: public wxObject
 {
   public:
-	wxSVGCanvas() { m_itemsCached = true; }
+	wxSVGCanvas():  m_itemsCached(true) {}
 	virtual ~wxSVGCanvas() {}
 	
 	virtual void Init(int width, int height) = 0;
@@ -35,30 +37,30 @@ class wxSVGCanvas: public wxObject
 	wxSVGCanvasItem* CreateItem(wxSVGEllipseElement* element);
 	wxSVGCanvasItem* CreateItem(wxSVGPathElement* element);
 	virtual wxSVGCanvasItem* CreateItem(wxSVGTextElement* element,
-	  wxCSSStyleDeclaration* style = NULL) = 0;
+	  const wxCSSStyleDeclaration* style = NULL) = 0;
 	wxSVGCanvasItem* CreateItem(wxSVGImageElement* element);
 	
-	virtual void DrawItem(wxSVGCanvasItem& item,
-	  wxSVGMatrix& matrix, wxCSSStyleDeclaration& style) = 0;
+	virtual void DrawItem(wxSVGCanvasItem& item, wxSVGMatrix& matrix,
+      const wxCSSStyleDeclaration& style, wxSVGSVGElement& svgElem) = 0;
     
 	void DrawLine(wxSVGLineElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawPolyline(wxSVGPolylineElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawPolygon(wxSVGPolygonElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawRect(wxSVGRectElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawCircle(wxSVGCircleElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawEllipse(wxSVGEllipseElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawPath(wxSVGPathElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawText(wxSVGTextElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	void DrawImage(wxSVGImageElement* element,
-	  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style = NULL);
+	  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style = NULL);
 	
 	inline bool IsItemsCached() { return m_itemsCached; }
 	inline void EnableItemsCache(bool enable = true) { m_itemsCached = enable; }
@@ -66,7 +68,25 @@ class wxSVGCanvas: public wxObject
   protected:
 	bool m_itemsCached;
     void DrawCanvasText(wxSVGCanvasText& canvasText, wxSVGMatrix& matrix,
-      wxCSSStyleDeclaration& style);
+      const wxCSSStyleDeclaration& style, wxSVGSVGElement& svgElem);
+	
+	unsigned int GetGradientStops(const wxSVGSVGElement& svgElem,
+      const wxString& href, float overall_opacity, const wxSVGElement*& refElem);
+	virtual void SetStopValue(unsigned int index, float offset, float opacity, const wxRGBColor& RGBColor) = 0;
+	virtual void AllocateGradientStops(unsigned int stop_count) = 0;
+	void GetLinearGradientVector(wxSVGPoint& p1, wxSVGPoint& p2, wxSVGMatrix& matrix, const wxSVGLinearGradientElement& gradElem);
+	void GetRadialGradientTransform(wxSVGPoint& Focus, wxSVGMatrix& matrix, const wxSVGRadialGradientElement& gradElem);
 };
+
+#define WX_SVG_CREATE_M_CANVAS_ITEM \
+  if (m_canvasItem == NULL)\
+	m_canvasItem = ((wxSVGDocument*) GetOwnerDocument())->GetCanvas()->CreateItem(this);
+
+#define WX_SVG_CLEAR_M_CANVAS_ITEM \
+  if (!((wxSVGDocument*) GetOwnerDocument())->GetCanvas()->IsItemsCached())\
+  {\
+	delete m_canvasItem;\
+	m_canvasItem = NULL;\
+  }
 
 #endif // WX_SVG_CANVAS_H
--- src/SVGCanvas.cpp
+++ src/SVGCanvas.cpp
@@ -3,7 +3,7 @@
 // Purpose:     wxSVGCanvas - Base class for SVG renders (backends)
 // Author:      Alex Thuering
 // Created:     2005/05/04
-// RCS-ID:      $Id: SVGCanvas.cpp,v 1.6 2005-11-17 17:51:27 ntalex Exp $
+// RCS-ID:      $Id: SVGCanvas.cpp,v 1.7 2006-01-07 20:44:09 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 //////////////////////////////////////////////////////////////////////////////
@@ -78,17 +78,15 @@ wxSVGCanvasItem* wxSVGCanvas::CreateItem(wxSVGImageElement* element)
 
 #define WX_SVG_DRAW(elem_name)\
 void wxSVGCanvas::Draw##elem_name(wxSVG##elem_name##Element* element,\
-  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style)\
+  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style)\
 {\
   wxSVGCanvasItem* canvasItem = CreateItem(element);\
   if (style == NULL)\
-	style = (wxCSSStyleDeclaration*) &element->GetStyle();\
-  DrawItem(*canvasItem, *matrix, *style);\
+	style = &element->GetStyle();\
+  if (style->GetDisplay() == wxCSS_VALUE_INLINE)\
+  	DrawItem(*canvasItem, *matrix, *style, *element->GetOwnerSVGElement());\
   if (IsItemsCached())\
-  {\
 	element->SetCanvasItem(canvasItem);\
-	element->SetScreenCTM(*matrix);\
-  }\
   else\
 	delete canvasItem;\
 }
@@ -103,29 +101,150 @@ WX_SVG_DRAW(Path)
 WX_SVG_DRAW(Image)
 
 void wxSVGCanvas::DrawText(wxSVGTextElement* element,
-  wxSVGMatrix* matrix, wxCSSStyleDeclaration* style)
+  wxSVGMatrix* matrix, const wxCSSStyleDeclaration* style)
 {
   if (style == NULL)
-	style = (wxCSSStyleDeclaration*) &element->GetStyle();
+	style = &element->GetStyle();
   wxSVGCanvasItem* canvasItem = CreateItem(element, style);
-  DrawItem(*canvasItem, *matrix, *style);
+  if (style->GetDisplay() == wxCSS_VALUE_INLINE)
+  	DrawItem(*canvasItem, *matrix, *style, *element->GetOwnerSVGElement());
   if (IsItemsCached())
-  {
 	element->SetCanvasItem(canvasItem);
-	element->SetScreenCTM(*matrix);
-  }
   else
 	delete canvasItem;
 }
 
 void wxSVGCanvas::DrawCanvasText(wxSVGCanvasText& canvasText,
-  wxSVGMatrix& matrix, wxCSSStyleDeclaration& style)
+  wxSVGMatrix& matrix, const wxCSSStyleDeclaration& style, wxSVGSVGElement& svgElem)
 {
   for (int i=0; i<(int)canvasText.m_chunks.Count(); i++)
   {
 	wxSVGCanvasTextChunk& chunk = canvasText.m_chunks[i];
 	chunk.style.SetFillRule(wxCSS_VALUE_EVENODD);
 	wxSVGMatrix pathMatrix = matrix.Multiply(chunk.matrix);
-	DrawItem(*chunk.path, pathMatrix, chunk.style);
+	DrawItem(*chunk.path, pathMatrix, chunk.style, svgElem);
   }
+}
+
+unsigned int wxSVGCanvas::GetGradientStops(const wxSVGSVGElement& svgElem,
+  const wxString& href, float overall_opacity, const wxSVGElement*& refElem)
+{
+    if (href.length() != 0 && href[0] == wxT('#'))
+    {
+        wxString tmp_href = href;
+        tmp_href.Remove(0,1);
+        refElem = (wxSVGSVGElement*) svgElem.GetElementById(tmp_href);
+  
+        // Linear gradient filling
+        if (refElem && (refElem->GetDtd() == wxSVG_LINEARGRADIENT_ELEMENT ||
+            refElem->GetDtd() == wxSVG_RADIALGRADIENT_ELEMENT))
+        {
+            // Search for the most referenced gradient
+            // (we assume that this is the one that contain stops)
+            wxSVGElement* tmp;
+            wxSVGLinearGradientElement* reference_gradient = (wxSVGLinearGradientElement*)refElem;
+            do {
+                tmp = NULL;
+                wxString grad_href = reference_gradient->GetHref();
+                if(	grad_href.Length() > 1 && grad_href[0] == wxT('#'))
+                {
+                    grad_href.Remove(0,1);
+                    tmp = (wxSVGSVGElement*) svgElem.GetElementById(grad_href);
+                    if (tmp && tmp->GetDtd() == wxSVG_LINEARGRADIENT_ELEMENT)
+                        reference_gradient = (wxSVGLinearGradientElement*)tmp;
+                }
+            } while(tmp);
+            
+            //Count number of stop element
+            wxSVGElement* stop_elem = (wxSVGElement*)reference_gradient->GetChildren();
+            unsigned int stop_count = 0; 
+            while (stop_elem)
+            {
+                if(stop_elem->GetDtd() == wxSVG_STOP_ELEMENT)
+                    stop_count++;
+                stop_elem = (wxSVGElement*)stop_elem->GetNext();
+            }
+            stop_elem = (wxSVGElement*)reference_gradient->GetChildren();
+  
+            // Allocate enough stops
+            AllocateGradientStops(stop_count);
+  
+            // Fill the stops
+            int i = 0;
+            while (stop_elem)
+            {
+                if(stop_elem->GetDtd() == wxSVG_STOP_ELEMENT)
+                {
+                    SetStopValue(
+                        i++,
+                        ((wxSVGStopElement*)stop_elem)->GetOffset(),
+                        ((wxSVGStopElement*)stop_elem)->GetStopOpacity() * overall_opacity,
+                        ((wxSVGStopElement*)stop_elem)->GetStopColor().GetRGBColor());
+                }
+                stop_elem = (wxSVGElement*)stop_elem->GetNext();
+            }
+            return stop_count;
+        }
+    }
+    return 0;
+}
+
+void wxSVGCanvas::GetLinearGradientVector(wxSVGPoint& p1, wxSVGPoint& p2, wxSVGMatrix& matrix, const wxSVGLinearGradientElement& gradElem)
+{
+
+//					TODO : implement gradientUnits parsing in SVGGradientElement setattribute
+//					double affine[6];
+//					if (rlg->obj_bbox) {
+//						affine[0] = ctx->x1 - ctx->x0;
+//						affine[1] = 0.;		
+//						affine[2] = 0.;
+//						affine[3] = ctx->y1 - ctx->y0;
+//						affine[4] = ctx->x0;
+//						affine[5] = ctx->y0;
+//				
+//					} else {
+//						for (i = 0; i < 6; i++)
+//							affine[i] = ctx->affine[i];
+//					}
+
+	// Compute gradient transformation matrix
+	wxSVGMatrix lg_matrix;
+	const wxSVGTransformList& transforms =  gradElem.GetGradientTransform().GetBaseVal();
+	for (int i=0; i<(int)transforms.Count(); i++)
+		lg_matrix = lg_matrix.Multiply(transforms[i].GetMatrix());
+
+	p1.SetX(gradElem.GetX1().GetBaseVal());
+	p1.SetY(gradElem.GetY1().GetBaseVal());
+	p2.SetX(gradElem.GetX2().GetBaseVal());
+	p2.SetY(gradElem.GetY2().GetBaseVal());
+	p1 = p1.MatrixTransform(lg_matrix);
+	p2 = p2.MatrixTransform(lg_matrix);
+	p1 = p1.MatrixTransform(matrix);
+	p2 = p2.MatrixTransform(matrix); 
+}
+
+
+void wxSVGCanvas::GetRadialGradientTransform(wxSVGPoint& Focus, wxSVGMatrix& matrix, const wxSVGRadialGradientElement& gradElem)
+{
+//					TODO : implement gradientUnits parsing in SVGGradientElement setattribute
+
+	// Compute gradient transformation matrix
+	wxSVGMatrix rg_matrix;
+	const wxSVGTransformList& transforms =  gradElem.GetGradientTransform().GetBaseVal();
+	for (int i=0; i<(int)transforms.Count(); i++)
+		rg_matrix = rg_matrix.Multiply(transforms[i].GetMatrix());
+	
+	// Apply Gradient parameters to transformation
+	double R, Cx, Cy, Fx, Fy;
+	R = (wxSVGLength)gradElem.GetR();
+	Cx = (wxSVGLength)gradElem.GetCx();
+	Cy = (wxSVGLength)gradElem.GetCy();
+	Fx = (wxSVGLength)gradElem.GetFx();
+	Fy = (wxSVGLength)gradElem.GetFy();
+	matrix = matrix.Multiply(rg_matrix).Translate(Cx,Cy).Scale(R);
+	
+	// Change Focus reference to gradient reference
+	Focus.SetX((Fx - Cx) / R);
+	Focus.SetY((Fy - Cy) / R);
+
 }
