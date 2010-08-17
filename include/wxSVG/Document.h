15c9b3003182fc6f4a40ba0e0f45c5da9446b9bc
Alex Thuering <alex@thuering.biz>
2010-08-17 08:00:29 +0000

    updated

--- include/wxSVG/Document.h
+++ include/wxSVG/Document.h
@@ -3,13 +3,17 @@
 // Purpose:     
 // Author:      Alex Thuering
 // Created:     2005/04/28
-// RCS-ID:      $Id: Document.h,v 1.3 2007-05-24 08:59:13 etisserant Exp $
+// RCS-ID:      $Id: Document.h,v 1.4 2010-08-17 08:00:29 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 //////////////////////////////////////////////////////////////////////////////
 
 #ifndef wxSVG_DOCUMENT_H
 #define wxSVG_DOCUMENT_H
+
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
 
 #include "wxSVGXML/svgxml.h"
 
