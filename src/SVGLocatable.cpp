f4c7582199ce404eb18974e5e94939be5c2ad925
Alex Thuering <alex@thuering.biz>
2014-03-24 21:16:35 +0000

    fixed GetBBox()

--- src/SVGLocatable.cpp
+++ src/SVGLocatable.cpp
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////
 // Name:        SVGLocatable.cpp
-// Purpose:     
+// Purpose:     Implementation of wxSVGLocatable
 // Author:      Alex Thuering
 // Created:     2005/05/10
-// RCS-ID:      $Id: SVGLocatable.cpp,v 1.11 2007-07-20 08:27:39 gusstdie Exp $
+// RCS-ID:      $Id: SVGLocatable.cpp,v 1.12 2014-03-24 21:16:35 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 //////////////////////////////////////////////////////////////////////////////
@@ -13,196 +13,145 @@
 #include <math.h>
 #include <wx/log.h>
 
-wxSVGRect wxSVGLocatable::GetElementBBox(const wxSVGElement* element,
-  wxSVG_COORDINATES coordinates)
-{
-  wxSVGRect elemBBox;
-  if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
-    return elemBBox;
-  // SVGLocatable
-  if (element->GetDtd() == wxSVG_SVG_ELEMENT)
-    return ((wxSVGSVGElement*)element)->GetBBox(coordinates);
-  // SVGTransformable
-  wxSVGTransformable* transformable =
-    wxSVGTransformable::GetSVGTransformable(*(wxSVGElement*)element);
-  if (!transformable){
-  		//printf("%f\n", GetCTM((wxSVGElement*)(element->GetParent())).GetE());
-      return elemBBox.MatrixTransform(GetCTM(element));
-  }
-  // GetBBox and transfrom it to parent coordinate system
-  if (coordinates != wxSVG_COORDINATES_USER)
-    return transformable->GetBBox(coordinates);
-    
-    
-  wxSVGMatrix matrix;
-  //matrix = GetCTM(element);
-   	/*
-  if (((wxSVGElement*)(element->GetParent()))->GetDtd() == wxSVG_G_ELEMENT){
- 		matrix = GetCTM((wxSVGElement*)(element->GetParent()->GetParent()));
- 		
-  }
- 	else
- 		matrix = GetCTM(element);*/
-  
-  //transformable->UpdateMatrix(matrix);
-  return transformable->GetBBox().MatrixTransform(matrix);
-  //return transformable->GetBBox();
+wxSVGRect wxSVGLocatable::GetElementBBox(const wxSVGElement* element, wxSVG_COORDINATES coordinates) {
+	wxSVGRect elemBBox;
+	if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
+		return elemBBox;
+	// SVGLocatable
+	if (element->GetDtd() == wxSVG_SVG_ELEMENT)
+		return ((wxSVGSVGElement*) element)->GetBBox(coordinates);
+	// SVGTransformable
+	wxSVGTransformable* transformable = wxSVGTransformable::GetSVGTransformable(*(wxSVGElement*) element);
+	if (!transformable) {
+		return elemBBox;
+	}
+	return transformable->GetBBox(coordinates);
 }
 
-wxSVGMatrix wxSVGLocatable::GetParentMatrix(const wxSVGElement* element)
-{
+wxSVGMatrix wxSVGLocatable::GetParentMatrix(const wxSVGElement* element) {
 	wxSVGMatrix matrix;
-	wxSvgXmlNode *parent = element->GetParentNode();
-	if (parent)
-	{
-		wxSVGElement *parent_element = parent->GetSvgElement();
-		if (parent_element) {
-			matrix = GetParentMatrix(parent_element);
-			wxSVGTransformable* transformableParent =
-    		wxSVGTransformable::GetSVGTransformable(*parent_element);
-    	if (transformableParent != NULL)
-  			transformableParent->UpdateMatrix(matrix);
+	wxSvgXmlNode* parent = element->GetParentNode();
+	if (parent) {
+		wxSVGElement* parentElement = parent->GetSvgElement();
+		if (parentElement) {
+			matrix = GetParentMatrix(parentElement);
+			wxSVGTransformable* transformableParent = wxSVGTransformable::GetSVGTransformable(*parentElement);
+			if (transformableParent != NULL)
+				transformableParent->UpdateMatrix(matrix);
 		}
 	}
 	return matrix;
 }
 
 
-wxSVGRect wxSVGLocatable::GetElementResultBBox(const wxSVGElement* element,
-  wxSVG_COORDINATES coordinates)
-{
-  wxSVGRect elemBBox;
-  if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
-    return elemBBox;
-  // SVGLocatable
-  if (element->GetDtd() == wxSVG_SVG_ELEMENT)
-    return ((wxSVGSVGElement*)element)->GetResultBBox(coordinates);
-  // SVGTransformable
-  wxSVGTransformable* transformable =
-    wxSVGTransformable::GetSVGTransformable(*(wxSVGElement*)element);
-  if (!transformable)
-      return elemBBox;
-  // GetResultBBox and transfrom it to parent coordinate system
-  if (coordinates != wxSVG_COORDINATES_USER)
-    return transformable->GetBBox(wxSVG_COORDINATES_USER);
-  wxSVGMatrix matrix;
-  //transformable->UpdateMatrix(matrix);
-  return transformable->GetResultBBox().MatrixTransform(matrix);
+wxSVGRect wxSVGLocatable::GetElementResultBBox(const wxSVGElement* element, wxSVG_COORDINATES coordinates) {
+	wxSVGRect elemBBox;
+	if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
+		return elemBBox;
+	// SVGLocatable
+	if (element->GetDtd() == wxSVG_SVG_ELEMENT)
+		return ((wxSVGSVGElement*) element)->GetResultBBox(coordinates);
+	// SVGTransformable
+	wxSVGTransformable* transformable = wxSVGTransformable::GetSVGTransformable(*(wxSVGElement*) element);
+	if (!transformable)
+		return elemBBox;
+	return transformable->GetResultBBox(coordinates);
 }
 
-wxSVGRect wxSVGLocatable::GetChildrenBBox(const wxSVGElement* element,
-  wxSVG_COORDINATES coordinates)
-{
-  wxSVGRect bbox;
-  wxSVGElement* child = (wxSVGElement*) element->GetChildren();
-  for (; child != NULL; child = (wxSVGElement*) child->GetNext())
-  {
-    wxSVGRect childBBox = wxSVGLocatable::GetElementBBox(child, coordinates);
-    if (childBBox.IsEmpty())
-      continue;
-    
-    if (bbox.IsEmpty())
-    {
-      bbox = childBBox;
-      continue;
-    }
-    
-    if (bbox.GetX() > childBBox.GetX())
-    {
-      bbox.SetWidth(bbox.GetWidth() + bbox.GetX() - childBBox.GetX());
-      bbox.SetX(childBBox.GetX());
-    }
-    if (bbox.GetY() > childBBox.GetY())
-    {
-      bbox.SetHeight(bbox.GetHeight() + bbox.GetY() - childBBox.GetY());
-      bbox.SetY(childBBox.GetY());
-    }
-    if (bbox.GetX() + bbox.GetWidth() < childBBox.GetX() + childBBox.GetWidth())
-      bbox.SetWidth(childBBox.GetX() + childBBox.GetWidth() - bbox.GetX());
-    if (bbox.GetY() + bbox.GetHeight() < childBBox.GetY() + childBBox.GetHeight())
-      bbox.SetHeight(childBBox.GetY() + childBBox.GetHeight() - bbox.GetY());
-  }
-  return bbox;
+wxSVGRect wxSVGLocatable::GetChildrenBBox(const wxSVGElement* element, wxSVG_COORDINATES coordinates) {
+	wxSVGRect bbox;
+	wxSVGElement* child = (wxSVGElement*) element->GetChildren();
+	for (; child != NULL; child = (wxSVGElement*) child->GetNext()) {
+		wxSVGRect childBBox = wxSVGLocatable::GetElementBBox(child, coordinates);
+		if (childBBox.IsEmpty())
+			continue;
+
+		if (bbox.IsEmpty()) {
+			bbox = childBBox;
+			continue;
+		}
+
+		if (bbox.GetX() > childBBox.GetX()) {
+			bbox.SetWidth(bbox.GetWidth() + bbox.GetX() - childBBox.GetX());
+			bbox.SetX(childBBox.GetX());
+		}
+		if (bbox.GetY() > childBBox.GetY()) {
+			bbox.SetHeight(bbox.GetHeight() + bbox.GetY() - childBBox.GetY());
+			bbox.SetY(childBBox.GetY());
+		}
+		if (bbox.GetX() + bbox.GetWidth() < childBBox.GetX() + childBBox.GetWidth())
+			bbox.SetWidth(childBBox.GetX() + childBBox.GetWidth() - bbox.GetX());
+		if (bbox.GetY() + bbox.GetHeight() < childBBox.GetY() + childBBox.GetHeight())
+			bbox.SetHeight(childBBox.GetY() + childBBox.GetHeight() - bbox.GetY());
+	}
+	return bbox;
 }
 
-wxSVGRect wxSVGLocatable::GetChildrenResultBBox(const wxSVGElement* element,
-  wxSVG_COORDINATES coordinates)
-{
-  wxSVGRect bbox;
-  wxSVGElement* child = (wxSVGElement*) element->GetChildren();
-  for (; child != NULL; child = (wxSVGElement*) child->GetNext())
-  {
-    wxSVGRect childBBox =
-      wxSVGLocatable::GetElementResultBBox(child, coordinates);
-    
-    if (childBBox.IsEmpty())
-      continue;
-    
-    if (bbox.IsEmpty())
-    {
-      bbox = childBBox;
-      continue;
-    }
-    
-    if (bbox.GetX() > childBBox.GetX())
-    {
-      bbox.SetWidth(bbox.GetWidth() + bbox.GetX() - childBBox.GetX());
-      bbox.SetX(childBBox.GetX());
-    }
-    if (bbox.GetY() > childBBox.GetY())
-    {
-      bbox.SetHeight(bbox.GetHeight() + bbox.GetY() - childBBox.GetY());
-      bbox.SetY(childBBox.GetY());
-    }
-    if (bbox.GetX() + bbox.GetWidth() < childBBox.GetX() + childBBox.GetWidth())
-      bbox.SetWidth(childBBox.GetX() + childBBox.GetWidth() - bbox.GetX());
-    if (bbox.GetY() + bbox.GetHeight() < childBBox.GetY() + childBBox.GetHeight())
-      bbox.SetHeight(childBBox.GetY() + childBBox.GetHeight() - bbox.GetY());
-  }
-  return bbox;
+wxSVGRect wxSVGLocatable::GetChildrenResultBBox(const wxSVGElement* element, wxSVG_COORDINATES coordinates) {
+	wxSVGRect bbox;
+	wxSVGElement* child = (wxSVGElement*) element->GetChildren();
+	for (; child != NULL; child = (wxSVGElement*) child->GetNext()) {
+		wxSVGRect childBBox = wxSVGLocatable::GetElementResultBBox(child, coordinates);
+
+		if (childBBox.IsEmpty())
+			continue;
+
+		if (bbox.IsEmpty()) {
+			bbox = childBBox;
+			continue;
+		}
+
+		if (bbox.GetX() > childBBox.GetX()) {
+			bbox.SetWidth(bbox.GetWidth() + bbox.GetX() - childBBox.GetX());
+			bbox.SetX(childBBox.GetX());
+		}
+		if (bbox.GetY() > childBBox.GetY()) {
+			bbox.SetHeight(bbox.GetHeight() + bbox.GetY() - childBBox.GetY());
+			bbox.SetY(childBBox.GetY());
+		}
+		if (bbox.GetX() + bbox.GetWidth() < childBBox.GetX() + childBBox.GetWidth())
+			bbox.SetWidth(childBBox.GetX() + childBBox.GetWidth() - bbox.GetX());
+		if (bbox.GetY() + bbox.GetHeight() < childBBox.GetY() + childBBox.GetHeight())
+			bbox.SetHeight(childBBox.GetY() + childBBox.GetHeight() - bbox.GetY());
+	}
+	return bbox;
 }
 
-wxSVGMatrix wxSVGLocatable::GetCTM(const wxSVGElement* element)
-{  
-  if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE ||
-      element->GetDtd() == wxSVG_SVG_ELEMENT)
-      return wxSVGMatrix();
-  wxSVGMatrix matrix = GetCTM((wxSVGElement*)(element->GetParent()));
-  const wxSVGTransformable* transformable =
-    wxSVGTransformable::GetSVGTransformable(*element);
-  if (!transformable)
-      return matrix;
-  transformable->UpdateMatrix(matrix);
-  return matrix;
+wxSVGMatrix wxSVGLocatable::GetCTM(const wxSVGElement* element) {
+	if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE
+			|| element->GetDtd() == wxSVG_SVG_ELEMENT)
+		return wxSVGMatrix();
+	wxSVGMatrix matrix = GetCTM((wxSVGElement*) (element->GetParent()));
+	const wxSVGTransformable* transformable =
+			wxSVGTransformable::GetSVGTransformable(*element);
+	if (!transformable)
+		return matrix;
+	transformable->UpdateMatrix(matrix);
+	return matrix;
 }
 
-wxSVGMatrix wxSVGLocatable::GetScreenCTM(const wxSVGElement* element)
-{  
-  if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
-      return wxSVGMatrix();
-  
-  wxSVGMatrix matrix;
-  if (element->GetParent())
-    matrix = GetScreenCTM((wxSVGElement*)(element->GetParent()));
-  else if (element->GetOwnerDocument())
-    matrix = ((wxSVGDocument*) element->GetOwnerDocument())->GetScreenCTM();
-  
-  if (element->GetDtd() == wxSVG_SVG_ELEMENT)
-  {
-    ((wxSVGSVGElement*)element)->UpdateMatrix(matrix);
-  }
-  else
-  {
-    const wxSVGTransformable* transformable =
-      wxSVGTransformable::GetSVGTransformable(*element);
-    if (transformable)
-      transformable->UpdateMatrix(matrix);
-  }
-  return matrix;
+wxSVGMatrix wxSVGLocatable::GetScreenCTM(const wxSVGElement* element) {
+	if (element == NULL || element->GetType() != wxSVGXML_ELEMENT_NODE)
+		return wxSVGMatrix();
+
+	wxSVGMatrix matrix;
+	if (element->GetParent())
+		matrix = GetScreenCTM((wxSVGElement*) (element->GetParent()));
+	else if (element->GetOwnerDocument())
+		matrix = ((wxSVGDocument*) element->GetOwnerDocument())->GetScreenCTM();
+
+	if (element->GetDtd() == wxSVG_SVG_ELEMENT) {
+		((wxSVGSVGElement*) element)->UpdateMatrix(matrix);
+	} else {
+		const wxSVGTransformable* transformable =
+				wxSVGTransformable::GetSVGTransformable(*element);
+		if (transformable)
+			transformable->UpdateMatrix(matrix);
+	}
+	return matrix;
 }
 
-wxSVGMatrix wxSVGLocatable::GetTransformToElement(const wxSVGElement& element)
-{
-  wxSVGMatrix res;
-  return res;
+wxSVGMatrix wxSVGLocatable::GetTransformToElement(const wxSVGElement& element) {
+	wxSVGMatrix res;
+	return res;
 }
--- src/SVGPathElement.cpp
+++ src/SVGPathElement.cpp
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////
 // Name:        SVGPathElement.cpp
-// Purpose:     
+// Purpose:     Implementation of wxSVGPathElement
 // Author:      Alex Thuering
 // Created:     2005/05/10
-// RCS-ID:      $Id: SVGPathElement.cpp,v 1.5 2007-07-20 08:27:39 gusstdie Exp $
+// RCS-ID:      $Id: SVGPathElement.cpp,v 1.6 2014-03-24 21:16:35 ntalex Exp $
 // Copyright:   (c) 2005 Alex Thuering
 // Licence:     wxWindows licence
 //////////////////////////////////////////////////////////////////////////////
@@ -12,165 +12,156 @@
 #include "SVGCanvas.h"
 #include <wx/log.h>
 
-wxSVGRect wxSVGPathElement::GetBBox(wxSVG_COORDINATES coordinates)
-{
-	wxSVGMatrix matrix = wxSVGLocatable::GetCTM(this);
-  WX_SVG_CREATE_M_CANVAS_ITEM
-  wxSVGRect bbox = coordinates == wxSVG_COORDINATES_USER ? 
-    m_canvasItem->GetBBox() : m_canvasItem->GetBBox(GetMatrix(coordinates));
-  WX_SVG_CLEAR_M_CANVAS_ITEM
-  return bbox.MatrixTransform(matrix);
+wxSVGRect wxSVGPathElement::GetBBox(wxSVG_COORDINATES coordinates) {
+	WX_SVG_CREATE_M_CANVAS_ITEM
+	wxSVGRect bbox = coordinates == wxSVG_COORDINATES_USER ? m_canvasItem->GetBBox() :
+			m_canvasItem->GetBBox(GetMatrix(coordinates));
+	WX_SVG_CLEAR_M_CANVAS_ITEM
+	return bbox;
 }
 
-wxSVGRect wxSVGPathElement::GetResultBBox(wxSVG_COORDINATES coordinates)
-{
-  wxCSSStyleDeclaration style = GetResultStyle(*this);
-  if (style.GetStroke().GetPaintType() == wxSVG_PAINTTYPE_NONE)
-    return GetBBox(coordinates);
-  WX_SVG_CREATE_M_CANVAS_ITEM
-  wxSVGRect bbox = coordinates == wxSVG_COORDINATES_USER ?
-    m_canvasItem->GetResultBBox(style) :
-    m_canvasItem->GetResultBBox(style, GetMatrix(coordinates));
-  WX_SVG_CLEAR_M_CANVAS_ITEM
-  return bbox;
+wxSVGRect wxSVGPathElement::GetResultBBox(wxSVG_COORDINATES coordinates) {
+	wxCSSStyleDeclaration style = GetResultStyle(*this);
+	if (style.GetStroke().GetPaintType() == wxSVG_PAINTTYPE_NONE)
+		return GetBBox(coordinates);
+	WX_SVG_CREATE_M_CANVAS_ITEM
+	wxSVGRect bbox = coordinates == wxSVG_COORDINATES_USER ? m_canvasItem->GetResultBBox(style) :
+			m_canvasItem->GetResultBBox(style, GetMatrix(coordinates));
+	WX_SVG_CLEAR_M_CANVAS_ITEM
+	return bbox;
 }
 
-void wxSVGPathElement::SetCanvasItem(wxSVGCanvasItem* canvasItem)
-{
-  if (m_canvasItem)
-    delete m_canvasItem;
-  m_canvasItem = canvasItem;
+void wxSVGPathElement::SetCanvasItem(wxSVGCanvasItem* canvasItem) {
+	if (m_canvasItem)
+		delete m_canvasItem;
+	m_canvasItem = canvasItem;
 }
 
-double wxSVGPathElement::GetTotalLength()
-{
-  double res = 0;
-  return res;
+double wxSVGPathElement::GetTotalLength() {
+	double res = 0;
+	return res;
 }
 
-wxSVGPoint wxSVGPathElement::GetPointAtLength(double distance)
-{
-  wxSVGPoint res;
-  return res;
+wxSVGPoint wxSVGPathElement::GetPointAtLength(double distance) {
+	wxSVGPoint res;
+	return res;
 }
 
-unsigned long wxSVGPathElement::GetPathSegAtLength(double distance)
-{
-  unsigned long res = 0;
-  return res;
+unsigned long wxSVGPathElement::GetPathSegAtLength(double distance) {
+	unsigned long res = 0;
+	return res;
 }
 
-wxSVGPathSegClosePath wxSVGPathElement::CreateSVGPathSegClosePath()
-{
-  wxSVGPathSegClosePath res;
-  return res;
+wxSVGPathSegClosePath wxSVGPathElement::CreateSVGPathSegClosePath() {
+	wxSVGPathSegClosePath res;
+	return res;
 }
 
-wxSVGPathSegMovetoAbs wxSVGPathElement::CreateSVGPathSegMovetoAbs(double x, double y)
-{
-  wxSVGPathSegMovetoAbs res;
-  return res;
+wxSVGPathSegMovetoAbs wxSVGPathElement::CreateSVGPathSegMovetoAbs(double x,
+		double y) {
+	wxSVGPathSegMovetoAbs res;
+	return res;
 }
 
-wxSVGPathSegMovetoRel wxSVGPathElement::CreateSVGPathSegMovetoRel(double x, double y)
-{
-  wxSVGPathSegMovetoRel res;
-  return res;
+wxSVGPathSegMovetoRel wxSVGPathElement::CreateSVGPathSegMovetoRel(double x,
+		double y) {
+	wxSVGPathSegMovetoRel res;
+	return res;
 }
 
-wxSVGPathSegLinetoAbs wxSVGPathElement::CreateSVGPathSegLinetoAbs(double x, double y)
-{
-  wxSVGPathSegLinetoAbs res;
-  return res;
+wxSVGPathSegLinetoAbs wxSVGPathElement::CreateSVGPathSegLinetoAbs(double x,
+		double y) {
+	wxSVGPathSegLinetoAbs res;
+	return res;
 }
 
-wxSVGPathSegLinetoRel wxSVGPathElement::CreateSVGPathSegLinetoRel(double x, double y)
-{
-  wxSVGPathSegLinetoRel res;
-  return res;
+wxSVGPathSegLinetoRel wxSVGPathElement::CreateSVGPathSegLinetoRel(double x,
+		double y) {
+	wxSVGPathSegLinetoRel res;
+	return res;
 }
 
-wxSVGPathSegCurvetoCubicAbs wxSVGPathElement::CreateSVGPathSegCurvetoCubicAbs(double x, double y, double x1, double y1, double x2, double y2)
-{
-  wxSVGPathSegCurvetoCubicAbs res;
-  return res;
+wxSVGPathSegCurvetoCubicAbs wxSVGPathElement::CreateSVGPathSegCurvetoCubicAbs(
+		double x, double y, double x1, double y1, double x2, double y2) {
+	wxSVGPathSegCurvetoCubicAbs res;
+	return res;
 }
 
-wxSVGPathSegCurvetoCubicRel wxSVGPathElement::CreateSVGPathSegCurvetoCubicRel(double x, double y, double x1, double y1, double x2, double y2)
-{
-  wxSVGPathSegCurvetoCubicRel res;
-  return res;
+wxSVGPathSegCurvetoCubicRel wxSVGPathElement::CreateSVGPathSegCurvetoCubicRel(
+		double x, double y, double x1, double y1, double x2, double y2) {
+	wxSVGPathSegCurvetoCubicRel res;
+	return res;
 }
 
-wxSVGPathSegCurvetoQuadraticAbs wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticAbs(double x, double y, double x1, double y1)
-{
-  wxSVGPathSegCurvetoQuadraticAbs res;
-  return res;
+wxSVGPathSegCurvetoQuadraticAbs wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticAbs(
+		double x, double y, double x1, double y1) {
+	wxSVGPathSegCurvetoQuadraticAbs res;
+	return res;
 }
 
-wxSVGPathSegCurvetoQuadraticRel wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticRel(double x, double y, double x1, double y1)
-{
-  wxSVGPathSegCurvetoQuadraticRel res;
-  return res;
+wxSVGPathSegCurvetoQuadraticRel wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticRel(
+		double x, double y, double x1, double y1) {
+	wxSVGPathSegCurvetoQuadraticRel res;
+	return res;
 }
 
-wxSVGPathSegArcAbs wxSVGPathElement::CreateSVGPathSegArcAbs(double x, double y, double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag)
-{
-  wxSVGPathSegArcAbs res;
-  return res;
+wxSVGPathSegArcAbs wxSVGPathElement::CreateSVGPathSegArcAbs(double x, double y,
+		double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag) {
+	wxSVGPathSegArcAbs res;
+	return res;
 }
 
-wxSVGPathSegArcRel wxSVGPathElement::CreateSVGPathSegArcRel(double x, double y, double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag)
-{
-  wxSVGPathSegArcRel res;
-  return res;
+wxSVGPathSegArcRel wxSVGPathElement::CreateSVGPathSegArcRel(double x, double y,
+		double r1, double r2, double angle, bool largeArcFlag, bool sweepFlag) {
+	wxSVGPathSegArcRel res;
+	return res;
 }
 
-wxSVGPathSegLinetoHorizontalAbs wxSVGPathElement::CreateSVGPathSegLinetoHorizontalAbs(double x)
-{
-  wxSVGPathSegLinetoHorizontalAbs res;
-  return res;
+wxSVGPathSegLinetoHorizontalAbs wxSVGPathElement::CreateSVGPathSegLinetoHorizontalAbs(
+		double x) {
+	wxSVGPathSegLinetoHorizontalAbs res;
+	return res;
 }
 
-wxSVGPathSegLinetoHorizontalRel wxSVGPathElement::CreateSVGPathSegLinetoHorizontalRel(double x)
-{
-  wxSVGPathSegLinetoHorizontalRel res;
-  return res;
+wxSVGPathSegLinetoHorizontalRel wxSVGPathElement::CreateSVGPathSegLinetoHorizontalRel(
+		double x) {
+	wxSVGPathSegLinetoHorizontalRel res;
+	return res;
 }
 
-wxSVGPathSegLinetoVerticalAbs wxSVGPathElement::CreateSVGPathSegLinetoVerticalAbs(double y)
-{
-  wxSVGPathSegLinetoVerticalAbs res;
-  return res;
+wxSVGPathSegLinetoVerticalAbs wxSVGPathElement::CreateSVGPathSegLinetoVerticalAbs(
+		double y) {
+	wxSVGPathSegLinetoVerticalAbs res;
+	return res;
 }
 
-wxSVGPathSegLinetoVerticalRel wxSVGPathElement::CreateSVGPathSegLinetoVerticalRel(double y)
-{
-  wxSVGPathSegLinetoVerticalRel res;
-  return res;
+wxSVGPathSegLinetoVerticalRel wxSVGPathElement::CreateSVGPathSegLinetoVerticalRel(
+		double y) {
+	wxSVGPathSegLinetoVerticalRel res;
+	return res;
 }
 
-wxSVGPathSegCurvetoCubicSmoothAbs wxSVGPathElement::CreateSVGPathSegCurvetoCubicSmoothAbs(double x, double y, double x2, double y2)
-{
-  wxSVGPathSegCurvetoCubicSmoothAbs res;
-  return res;
+wxSVGPathSegCurvetoCubicSmoothAbs wxSVGPathElement::CreateSVGPathSegCurvetoCubicSmoothAbs(
+		double x, double y, double x2, double y2) {
+	wxSVGPathSegCurvetoCubicSmoothAbs res;
+	return res;
 }
 
-wxSVGPathSegCurvetoCubicSmoothRel wxSVGPathElement::CreateSVGPathSegCurvetoCubicSmoothRel(double x, double y, double x2, double y2)
-{
-  wxSVGPathSegCurvetoCubicSmoothRel res;
-  return res;
+wxSVGPathSegCurvetoCubicSmoothRel wxSVGPathElement::CreateSVGPathSegCurvetoCubicSmoothRel(
+		double x, double y, double x2, double y2) {
+	wxSVGPathSegCurvetoCubicSmoothRel res;
+	return res;
 }
 
-wxSVGPathSegCurvetoQuadraticSmoothAbs wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticSmoothAbs(double x, double y)
-{
-  wxSVGPathSegCurvetoQuadraticSmoothAbs res;
-  return res;
+wxSVGPathSegCurvetoQuadraticSmoothAbs wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticSmoothAbs(
+		double x, double y) {
+	wxSVGPathSegCurvetoQuadraticSmoothAbs res;
+	return res;
 }
 
-wxSVGPathSegCurvetoQuadraticSmoothRel wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticSmoothRel(double x, double y)
-{
-  wxSVGPathSegCurvetoQuadraticSmoothRel res;
-  return res;
+wxSVGPathSegCurvetoQuadraticSmoothRel wxSVGPathElement::CreateSVGPathSegCurvetoQuadraticSmoothRel(
+		double x, double y) {
+	wxSVGPathSegCurvetoQuadraticSmoothRel res;
+	return res;
 }
 
