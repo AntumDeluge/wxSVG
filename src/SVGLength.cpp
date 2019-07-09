59fae1eefa1208aeb2b3a885724078d9aa52015b
ntalex <ntalex@sf.net>
2019-07-09 11:43:15 +0200

    fixed SetValueInSpecifiedUnits()

--- src/SVGLength.cpp
+++ src/SVGLength.cpp
@@ -10,11 +10,9 @@
 
 #include "SVGLength.h"
 
-wxString wxSVGLength::GetValueAsString() const
-{
+wxString wxSVGLength::GetValueAsString() const {
   wxString value = wxString::Format(wxT("%g"), m_valueInSpecifiedUnits);
-  switch (m_unitType)
-  {
+  switch (m_unitType) {
 	case wxSVG_LENGTHTYPE_UNKNOWN: break;
 	case wxSVG_LENGTHTYPE_NUMBER: break;
 	case wxSVG_LENGTHTYPE_PX: value += wxT("px"); break;
@@ -30,85 +28,76 @@ wxString wxSVGLength::GetValueAsString() const
   return value;
 }
 
-void wxSVGLength::SetValueAsString(const wxString& n)
-{
-  m_valueInSpecifiedUnits = 0;
-  m_unitType = wxSVG_LENGTHTYPE_NUMBER;
-  wxString value = n.Strip(wxString::both);
-  wxString unit;
-  if (value.length()>=2)
-  {
-	const wxString s_numeric = wxT("0123456789");
-	const wxString s_numericFirst = wxT("+-.Ee") + s_numeric;
-	if (!s_numeric.Contains(value.Right(1)))
-	{
-	  if (s_numericFirst.Contains(value.Mid(value.Length()-2,1)))
-	  {
-		unit = value.Right(1);
-		value = value.Left(value.Length()-1);
-	  }
-	  else
-	  {
-		unit = value.Right(2);
-		value = value.Left(value.Length()-2);
-	  }
+void wxSVGLength::SetValueAsString(const wxString& n) {
+	m_valueInSpecifiedUnits = 0;
+	m_unitType = wxSVG_LENGTHTYPE_NUMBER;
+	wxString value = n.Strip(wxString::both);
+	wxString unit;
+	if (value.length() >= 2) {
+		const wxString s_numeric = wxT("0123456789");
+		const wxString s_numericFirst = wxT("+-.Ee") + s_numeric;
+		if (!s_numeric.Contains(value.Right(1))) {
+			if (s_numericFirst.Contains(value.Mid(value.Length() - 2, 1))) {
+				unit = value.Right(1);
+				value = value.Left(value.Length() - 1);
+			} else {
+				unit = value.Right(2);
+				value = value.Left(value.Length() - 2);
+			}
+		}
 	}
-  }
-  
-  double d;
-  if (!value.ToDouble(&d))
-	return;
-  m_valueInSpecifiedUnits = d;
-  
-  if (unit.length() == 0);
-  else if (unit == wxT("px"))
-	m_unitType = wxSVG_LENGTHTYPE_PX;
-  else if (unit.Right(1) == wxT("%"))
-	m_unitType = wxSVG_LENGTHTYPE_PERCENTAGE;
-  else if (unit == wxT("em"))
-	m_unitType = wxSVG_LENGTHTYPE_EMS;
-  else if (unit == wxT("ex"))
-	m_unitType = wxSVG_LENGTHTYPE_EXS;
-  else if (unit == wxT("cm"))
-	m_unitType = wxSVG_LENGTHTYPE_CM;
-  else if (unit == wxT("mm"))
-	m_unitType = wxSVG_LENGTHTYPE_MM;
-  else if (unit == wxT("in"))
-	m_unitType = wxSVG_LENGTHTYPE_IN;
-  else if (unit == wxT("pt"))
-	m_unitType = wxSVG_LENGTHTYPE_PT;
-  else if (unit == wxT("pc"))
-	m_unitType = wxSVG_LENGTHTYPE_PC;
-  SetValueInSpecifiedUnits(m_valueInSpecifiedUnits);
+
+	double d;
+	if (!value.ToDouble(&d))
+		return;
+	m_valueInSpecifiedUnits = d;
+
+	if (unit.length() > 0) {
+		if (unit == wxT("px"))
+			m_unitType = wxSVG_LENGTHTYPE_PX;
+		else if (unit.Right(1) == wxT("%"))
+			m_unitType = wxSVG_LENGTHTYPE_PERCENTAGE;
+		else if (unit == wxT("em"))
+			m_unitType = wxSVG_LENGTHTYPE_EMS;
+		else if (unit == wxT("ex"))
+			m_unitType = wxSVG_LENGTHTYPE_EXS;
+		else if (unit == wxT("cm"))
+			m_unitType = wxSVG_LENGTHTYPE_CM;
+		else if (unit == wxT("mm"))
+			m_unitType = wxSVG_LENGTHTYPE_MM;
+		else if (unit == wxT("in"))
+			m_unitType = wxSVG_LENGTHTYPE_IN;
+		else if (unit == wxT("pt"))
+			m_unitType = wxSVG_LENGTHTYPE_PT;
+		else if (unit == wxT("pc"))
+			m_unitType = wxSVG_LENGTHTYPE_PC;
+	}
+	SetValueInSpecifiedUnits(m_valueInSpecifiedUnits);
 }
 
-void wxSVGLength::NewValueSpecifiedUnits(wxSVG_LENGTHTYPE unitType, double valueInSpecifiedUnits)
-{
-  m_unitType = unitType;
-  SetValueInSpecifiedUnits(valueInSpecifiedUnits);
+void wxSVGLength::NewValueSpecifiedUnits(wxSVG_LENGTHTYPE unitType, double valueInSpecifiedUnits) {
+	m_unitType = unitType;
+	SetValueInSpecifiedUnits(valueInSpecifiedUnits);
 }
 
-void wxSVGLength::ConvertToSpecifiedUnits(wxSVG_LENGTHTYPE unitType)
-{
-  m_unitType = unitType;
+void wxSVGLength::ConvertToSpecifiedUnits(wxSVG_LENGTHTYPE unitType) {
+	m_unitType = unitType;
 }
 
-void wxSVGLength::SetValueInSpecifiedUnits(double n)
-{
-  m_valueInSpecifiedUnits = n;
-  m_value = n;
-  switch (m_unitType)
-  {
-	case wxSVG_LENGTHTYPE_UNKNOWN: break;
-	case wxSVG_LENGTHTYPE_NUMBER: break;
-	case wxSVG_LENGTHTYPE_PX: break;
-	case wxSVG_LENGTHTYPE_PERCENTAGE: break; // todo
-	case wxSVG_LENGTHTYPE_EMS: break; // todo
-	case wxSVG_LENGTHTYPE_EXS: break; // todo
-	case wxSVG_LENGTHTYPE_CM: m_value *= 35.43307; break;
-	case wxSVG_LENGTHTYPE_MM: m_value *= 3.543307; break;
-	case wxSVG_LENGTHTYPE_IN: m_value *= 90; break;
-	case wxSVG_LENGTHTYPE_PT: m_value *= 1.25; break;
-	case wxSVG_LENGTHTYPE_PC: m_value *= 15; break;
-  }
+void wxSVGLength::SetValueInSpecifiedUnits(double n) {
+	m_valueInSpecifiedUnits = n;
+	m_value = n;
+	switch (m_unitType) {
+		case wxSVG_LENGTHTYPE_UNKNOWN: break;
+		case wxSVG_LENGTHTYPE_NUMBER: break;
+		case wxSVG_LENGTHTYPE_PX: break;
+		case wxSVG_LENGTHTYPE_PERCENTAGE: break; // todo
+		case wxSVG_LENGTHTYPE_EMS: break; // todo
+		case wxSVG_LENGTHTYPE_EXS: break; // todo
+		case wxSVG_LENGTHTYPE_CM: m_value *= 35.43307; break;
+		case wxSVG_LENGTHTYPE_MM: m_value *= 3.543307; break;
+		case wxSVG_LENGTHTYPE_IN: m_value *= 90; break;
+		case wxSVG_LENGTHTYPE_PT: m_value = m_value * 96 / 72; break;
+		case wxSVG_LENGTHTYPE_PC: m_value *= 15; break;
+	}
 }
